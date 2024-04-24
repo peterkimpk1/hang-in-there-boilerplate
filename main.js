31af69a3d7e526959cc7e79d2fcd7d2f70ae292c
Peter Kim <peterkim.pk1@gmail.com>
2024-04-24 15:25:43 -0400

    Moved main.js

---
+++ main.js
@@ -0,0 +1,229 @@
+// query selector variables go here 👇
+var image = document.querySelector('.poster-img');
+var savePosterButton = document.querySelector('.save-poster');
+var showSavePosterButton = document.querySelector('.show-saved');
+var showRandomPosterButton = document.querySelector('.show-random');
+var makePosterButton = document.querySelector('.show-form');
+var posterTitle = document.querySelector('.poster-title');
+var posterQuote = document.querySelector('.poster-quote');
+var mainPage = document.querySelector('.main-poster');
+var createOwnPoster = document.querySelector('.poster-form');
+var takeMeBackButton = document.querySelector('.show-main')
+var savedPosterPage = document.querySelector('.saved-posters')
+var backToMainButton = document.querySelector('.back-to-main')
+var inputURL = document.querySelector('#poster-image-url')
+var inputTitle = document.querySelector('#poster-title')
+var inputQuote = document.querySelector('#poster-quote')
+var showMyPosterButton = document.querySelector('.make-poster')
+var savedPostersGrid = document.querySelector('.saved-posters-grid')
+
+
+// we've provided you with some data to work with 👇
+var images = [
+  "./assets/bees.jpg",
+  "./assets/bridge.jpg",
+  "./assets/butterfly.jpg",
+  "./assets/cliff.jpg",
+  "./assets/elephant.jpg",
+  "./assets/flock.jpg",
+  "./assets/fox.jpg",
+  "./assets/frog.jpg",
+  "./assets/horse.jpg",
+  "./assets/lion.jpg",
+  "./assets/mountain.jpg",
+  "./assets/pier.jpg",
+  "./assets/puffins.jpg",
+  "./assets/pug.jpg",
+  "./assets/runner.jpg",
+  "./assets/squirrel.jpg",
+  "./assets/tiger.jpg",
+  "./assets/turtle.jpg"
+];
+var titles = [
+  "determination",
+  "success",
+  "inspiration",
+  "perspiration",
+  "grit",
+  "empathy",
+  "feelings",
+  "hope",
+  "believe",
+  "try",
+  "conviction",
+  "accomplishment",
+  "achievement",
+  "ambition",
+  "clarity",
+  "challenge",
+  "commitment",
+  "confidence",
+  "action",
+  "courage",
+  "focus",
+  "breathe",
+  "gratitude",
+  "imagination",
+  "kindness",
+  "mindfulness",
+  "knowledge",
+  "opportunity",
+  "passion",
+  "patience",
+  "practice",
+  "smile",
+  "trust",
+  "understanding",
+  "wisdom"
+];
+var quotes = [
+  "Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
+  "You are braver than you believe, stronger than you seem and smarter than you think.",
+  "You are confined only by the walls you build yourself.",
+  "The one who has confidence gains the confidence of others.",
+  "Act as if what you do makes a difference. It does.",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "Never bend your head. Always hold it high. Look the world straight in the eye.",
+  "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+  "Believe you can and you're halfway there.",
+  "When you have a dream, you've got to grab it and never let go.",
+  "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+  "No matter what you're going through, there's a light at the end of the tunnel.",
+  "It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.",
+  "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  "Just don't give up trying to do what you really want to do. Where there is love and inspiration, I don't think you can go wrong.",
+  'Limit your "always" and your "nevers."',
+  "You are never too old to set another goal or to dream a new dream.",
+  "Try to be a rainbow in someone else's cloud.",
+  "You do not find the happy life. You make it.",
+  "Inspiration comes from within yourself. One has to be positive. When you're positive, good things happen.",
+  "Sometimes you will never know the value of a moment, until it becomes a memory.",
+  "The most wasted of days is one without laughter.",
+  "You must do the things you think you cannot do.",
+  "It isn't where you came from. It's where you're going that counts.",
+  "It is never too late to be what you might have been.",
+  "Happiness often sneaks in through a door you didn't know you left open.",
+  "We must be willing to let go of the life we planned so as to have the life that is waiting for us.",
+  "Never limit yourself because of others’ limited imagination; never limit others because of your own limited imagination.",
+  "Be the change that you wish to see in the world.",
+  "Let us make our future now, and let us make our dreams tomorrow's reality.",
+  "You don't always need a plan. Sometimes you just need to breathe, trust, let go, and see what happens.",
+  "If I cannot do great things, I can do small things in a great way.",
+  "Don't wait. The time will never be just right.",
+  "With the right kind of coaching and determination you can accomplish anything.",
+  "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
+  "No matter what people tell you, words and ideas can change the world.",
+  "Each person must live their life as a model for others.",
+  "A champion is defined not by their wins but by how they can recover when they fall."
+];
+var savedPosters = [];
+var currentPoster;
+// event listeners go here 👇
+showRandomPosterButton.addEventListener('click', changePoster);
+makePosterButton.addEventListener('click', makeYourOwnPosterPage);
+takeMeBackButton.addEventListener('click', takeMeBackToMainPage);
+showSavePosterButton.addEventListener('click', mainPageToSavedPosters);
+backToMainButton.addEventListener('click', savedToMain);
+showMyPosterButton.addEventListener('click', showMyPoster);
+savePosterButton.addEventListener('click', savePoster);
+savedPostersGrid.addEventListener('dblclick', deletePoster);
+// functions and event handlers go here 👇
+function deletePoster() {
+  if (event.target.classList.contains('mini-poster')) {
+    event.target.parentNode.remove();
+  }
+  toggleButton();
+} 
+
+function savePoster() {
+  var savedPoster = createPoster(image.src, posterTitle.innerText, posterQuote.innerText);
+  savedPosters.push(savedPoster);
+  addToSavedPostersGrid();
+  currentPoster = savedPoster;
+  toggleButton();
+}
+
+function addToSavedPostersGrid() {
+  savedPostersGrid.innerHTML += `
+    <article class="mini-poster">
+      <img class="mini-poster img" src="${image.src}"/>
+      <div id="mini-poster h2">${posterTitle.innerText}</div>
+      <div id="poster-quote">${posterQuote.innerText}</div>
+    </article>`;
+}
+
+function toggleButton() {
+  for (i = 0; i < savedPosters.length; i++) {
+    if (savedPosters[i] === (currentPoster)) {
+      savePosterButton.setAttribute("disabled", "");
+    }
+    else if (savedPosters[i] !== (currentPoster)) {
+      savePosterButton.removeAttribute("disabled");
+    }
+}
+}
+
+function showMyPoster(event) {
+  event.preventDefault();
+  if (inputURL.value !== undefined) {
+    image.src = inputURL.value;
+    images.push(inputURL.value);
+  }
+  if (inputTitle.value !== undefined) {
+    posterTitle.innerText = inputTitle.value;
+    titles.push(inputTitle.value);
+  }
+  if (inputQuote.value !== undefined) {
+    posterQuote.innerText = inputQuote.value;
+    quotes.push(inputQuote.value);
+  }
+  var newPoster = createPoster(inputURL.value, inputTitle.value, inputQuote.value);
+  currentPoster = newPoster;
+  takeMeBackToMainPage();
+  toggleButton();
+}
+
+function savedToMain() {
+  savedPosterPage.classList.add('hidden');
+  mainPage.classList.remove('hidden');
+}
+
+function mainPageToSavedPosters() {
+  mainPage.classList.add('hidden');
+  savedPosterPage.classList.remove('hidden');
+}
+
+function takeMeBackToMainPage() {
+  createOwnPoster.classList.add('hidden');
+  mainPage.classList.remove('hidden');
+}
+
+function makeYourOwnPosterPage() {
+  mainPage.classList.add('hidden');
+  createOwnPoster.classList.remove('hidden');
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function createPoster(imageURL, title, quote) {
+  return {
+    id: Date.now(), 
+    imageURL: imageURL, 
+    title: title, 
+    quote: quote}
+}
+
+function changePoster() {
+  var randomImage = images[getRandomIndex(images)];
+  image.src = randomImage;
+  var randomTitle = titles [getRandomIndex (titles)];
+  posterTitle.innerText = randomTitle;
+  var randomQuote = quotes [getRandomIndex (quotes)];
+  posterQuote.innerText = randomQuote;
+  currentPoster = createPoster(randomImage, randomTitle, randomQuote);
+  toggleButton();
+}
+
+changePoster();
